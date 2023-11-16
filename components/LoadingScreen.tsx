2246eae257f49f98c50e5179ea107be9e0bebed5
aroogit commit -m First <arek.dec@gmail.com>
2023-11-16 22:50:17 +0100

    change name

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -7,7 +7,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ onClick }) => {
     <div className="bg-black inset-0 w-full h-full flex justify-center items-center flex-col fixed z-[100]">
       <div className="absolute inset-0 opacity-20 overflow-hidden ">
         <video
-          src="/shorts/intro.mp4"
+          src="/shorts/Intro.mp4"
           autoPlay
           muted
           loop
